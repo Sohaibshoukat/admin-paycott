25be5ca75a4e0eb8f315bffba2990b85365d6c4a
iamshahid65 <143092464+iamshahid65@users.noreply.github.com>
2024-06-28 23:12:43 +0500

    Pushing created new Tables on Active and Archive Page

--- src/Pages/Views/AdvertisingCenter.jsx/ActiveAdd.jsx
+++ src/Pages/Views/AdvertisingCenter.jsx/ActiveAdd.jsx
@@ -1,9 +1,112 @@
 import React, { useState } from 'react'
 import { ServiceListData } from '../../../assets/Data/SubCategory'
 import { HiDotsVertical } from 'react-icons/hi'
+import { useNavigate } from 'react-router-dom';
 
 const ActiveAdd = () => {
-    const [ActionModel, setActionModel] = useState(null)
+    const [ActionModel, setActionModel] = useState(null);
+    const navigate=useNavigate()
+    const Data = [
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Marketplace',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "ACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'Yes',
+            views: '4567899',
+            sales: '85',
+            status: "InACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Marketplace',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "ACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "InACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "ACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "ACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "InACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'no',
+            views: '4567899',
+            sales: '85',
+            status: "ACTIVE"
+        },
+        {
+            adid: '43567',
+            date: '10 DEC 2024',
+            Marketplace: 'Services',
+            Category: 'Logo & Brand->',
+            useId: '45678',
+            boosted: 'yes',
+            views: '4567899',
+            sales: '85',
+            status: "ACTIVE"
+        },
+    ]
 
     return (
         <div>
@@ -38,67 +141,70 @@ const ActiveAdd = () => {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-4 gap-y-6 my-6">
-                {ServiceListData?.map((item, index) => (
-                    <div
-                        className="bg-white rounded-2xl flex flex-col gap-4 font-pop border-2 border-light py-2 px-4"
-                        key={index}
-                    >
-                        <div className="flex flex-row justify-between">
-                            <div className="flex gap-2">
-                                <div>
-                                    <img src={item.img} alt="" className='w-12 h-12 rounded-full' />
-                                </div>
-                                <div className="flex flex-col gap-0">
-                                    <div className="flex flex-row gap-1 items-center">
-                                        {item?.badge && <img src="../../Assets/Prop/badge.png" className='w-4 h-4' />}
-                                        <h4 className='text-lightGrey'>Level {item.level}</h4>
-                                    </div>
-                                    <h3 className='text-lg font-semibold'>{item.Name}</h3>
-                                </div>
-                            </div>
-                            <div className="flex items-center gap-2">
-                                <div
-                                    className="relative"
-                                    onClick={() => {
-                                        if (index == ActionModel) {
-                                            setActionModel(null)
-                                        } else {
-                                            setActionModel(index)
-                                        }
-                                    }}
-                                >
-                                    <HiDotsVertical className='text-2xl text-black' />
-                                    {ActionModel == index && 
-                                    <div className="bg-white rounded-lg w-max absolute top-[100%] right-0 py-2 px-6 font-pop text-lg font-medium shadow-xl">
-                                        <h2>Close Ad</h2>
-                                        <h2>Delete Ad</h2>
-                                    </div>}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="flex flex-row font-pop gap-1">
-                            {item.List.slice(0, 2).map((item2, index2) => (
-                                <div className='bg-[#EFFBF1] rounded-full py-1 px-2 text-sm' key={index2}>
-                                    {item2}
-                                </div>
-                            ))}
-                            {item?.List?.length > 2 && (
-                                <div className='bg-[#EFFBF1] rounded-full p-1 text-sm'>
-                                    +{item.List?.length - 2}
-                                </div>
-                            )}
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <h2 className='font-medium '>{item?.Des}</h2>
-                            <div className="flex w-full flex-row gap-1">
-                                {item.Sample.map((item2, index2) => (
-                                    <img src={item2} alt="" className='w-[33%]' key={index2} />
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                ))}
+            <div className='bg-transparent overflow-x-auto font-pop w-full my-2 md:my-6'>
+                <table className='w-full border-spacing-y-4 border-separate'>
+                    <thead>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">All</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">Ad ID</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">DATE</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">MARKETPLACE</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">CATEGORY</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">User-ID</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">Boosted</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">Views</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">Sales</th>
+                        <th scope="col" class="px-2 md:px-6 py-1 md:py-3 text-left font-medium text-gray-500 uppercase">Status</th>
+                    </thead>
+                    <tbody>
+                        {Data?.map((item, index) => (
+                            <tr key={index} className='cursor-pointer'>
+                                <td class="px-2 rounded-l-2xl md:px-6 bg-white my-3 text-base md:text-lg font-semibold py-3 text-left w-max min-w-[30%]">
+                                    <div className="flex font-medium w-max flex-row gap-2 items-center">
+                                        <button
+                                            onClick={() => {
+                                                if (item?.Marketplace == "Marketplace") {
+                                                    navigate("/admin-dashboard/add-center/market-add-detail")
+                                                } else {
+                                                    navigate("/admin-dashboard/add-center/service-add-detail")
+                                                }
+                                            }}
+                                            className='bg-primarygreen border-2 border-primarygreen py-2 px-4 text-white font-pop rounded-full hover:bg-transparent hover:text-primarygreen ease-in-out duration-300 w-max'
+                                        >
+                                            View Detail
+                                        </button>                                    </div>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left ">
+                                    <h2 className='w-max'>{item?.adid}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left ">
+                                    <h2 className='w-max'>{item?.date}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left ">
+                                    <h2 className='w-max'>{item?.Marketplace}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left ">
+                                    <h2 className='w-max'>{item?.Category}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left ">
+                                    <h2 className='w-max'>{item?.useId}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 text-left font-semibold ">
+                                    <h2 className={`'w-max' ${item.boosted=="no"?'text-red-600':'text-green-600'}`}>{item?.boosted}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 text-left ">
+                                    <h2 className='w-max'>{item?.views}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left ">
+                                    <h2 className='w-max'>{item?.sales}</h2>
+                                </td>
+                                <td class="px-2 md:px-6 bg-white my-3 py-1 md:py-3 font-normal text-left rounded-r-xl ">
+                                    <h2 className={`'w-max' ${item.status=="ACTIVE"?'text-green-600':'text-red-600'}`}>{item?.status}</h2>
+                                </td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     )
